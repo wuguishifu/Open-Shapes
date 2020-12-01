0852091128894b1770dbf121d665a534ef686014
Bo Bramer <bramer.bo@gmail.com>
2020-12-01 04:10:48 -0800

    Fixed an issue that occasionally caused circles to not render properly. Changed default amount of triangles from 20 to 120 to occasionally fix an issue where circles on some planes would not render vertices correctly.

--- src/com/bramerlabs/shapes3d/Cylinder.java
+++ src/com/bramerlabs/shapes3d/Cylinder.java
@@ -1,4 +1,15 @@
 package com.bramerlabs.shapes3d;
 
+import com.bramerlabs.support.Vector3f;
+
 public class Cylinder {
+
+    // generate a cylinder using triangles from two points and radius
+
+    // the center of the two circular faces
+    private Vector3f p1;
+    private Vector3f p2;
+
+    // test comment
+
 }
